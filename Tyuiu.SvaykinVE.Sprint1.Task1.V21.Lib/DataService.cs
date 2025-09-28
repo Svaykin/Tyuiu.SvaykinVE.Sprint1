05cf0665fdcb365f589129313fd1d3108627f250
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-09-28 19:08:08 +0500

    Task1

---
+++ Tyuiu.SvaykinVE.Sprint1.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SvaykinVE.Sprint1.Task1.V21.Lib
+{
+    public class DataService : ISprint1Task1V21
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x * y) / (3 + y);
+        }
+    }
+
+}
+    
---
+++ Tyuiu.SvaykinVE.Sprint1.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SvaykinVE.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.SvaykinVE.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.4, res);
+        }
+    }
+}
