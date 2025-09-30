080f9cfd2a3a887ac48a2d398a36c6c659f1f00b
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-09-30 20:21:45 +0500

    Task6

---
+++ Tyuiu.SvaykinVE.Sprint1.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SvaykinVE.Sprint1.Task6.V3.Lib;
+namespace Tyuiu.SvaykinVE.Sprint1.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+            string x = "здарова надоел лол кама";
+            string res = ds.LastLetterWord(x);
+            Assert.AreEqual("алла", res);
+        }
+    }
+}
---
+++ Tyuiu.SvaykinVE.Sprint1.Task6.V3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.SvaykinVE.Sprint1.Task6.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Свайкин В. Е. | AСОиУб-25-1";
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* Спринт #1                                                                         *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                                  *");
+        Console.WriteLine("* Задание #5                                                                        *");
+        Console.WriteLine("* Вариант #3                                                                        *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | AСОиУб-25-1                              *");
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст.                                    *");
+        Console.WriteLine("* Напечатать строку, составленную из последних литер всех слов.                     *");
+        Console.WriteLine("*                                                                                   *");
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+        Console.WriteLine("*************************************************************************************");
+
+        string x;
+        Console.WriteLine("Напечатайте строку ");
+        x = Console.ReadLine();
+
+
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                                         *");
+        Console.WriteLine("*************************************************************************************");
+
+        Console.WriteLine(ds.LastLetterWord(x));
+        Console.ReadLine();
+    }
+}
